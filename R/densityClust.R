9e9ea8262b03fef7b3c0733ab14a8d67228bc0c2
James Manton <ajd.manton@googlemail.com>
2014-08-20 16:54:12 +0100

    Update documentation for densityClust functions

--- R/densityClust.R
+++ R/densityClust.R
@@ -1,23 +1,19 @@
 #' Computes the local density of points in a distance matrix
 #' 
-#' This function takes a distance matrix and a distance cutoff and calculate the
-#' local density for each point in the matrix. The computation can either be 
-#' done using a simple summation of the points with the distance cutoff for each
-#' observation, or by applying a gaussian kernel scaled by the distance cutoff 
-#' (more robust for low-density data)
+#' This function takes a distance matrix and a distance cutoff and calculates
+#' the local density for each point in the matrix. The computation can either be
+#' done using a simple summation of the points within the distance cutoff for
+#' each point, or by applying a Gaussian kernel scaled by the distance cutoff 
+#' (more robust for low-density data).
 #' 
-#' @param distance A distance matrix
-#' 
-#' @param dc A numeric value specifying the distance cutoff
-#' 
-#' @param gaussian Logical. Should a gaussian kernel be used to estimate the 
-#' density (defaults to FALSE)
-#' 
-#' @return A vector of local density values, the index matching row and column 
-#' indexes in the distance matrix
-#' 
-#' @noRd
-#' 
+#' @param distance a distance matrix.
+#' @param dc a numeric value specifying the distance cutoff.
+#' @param gaussian logical indicating that a Gaussian kernel should be used to
+#'   estimate the density.
+#'   
+#' @return A vector of local density values, one for each point.
+#'   
+#' @export
 localDensity <- function(distance, dc, gaussian=FALSE) {
     comb <- as.matrix(distance)
     if(gaussian) {
@@ -32,19 +28,16 @@ localDensity <- function(distance, dc, gaussian=FALSE) {
     }
     res
 }
+
+
 #' Calculate distance to closest observation of higher density
 #' 
-#' This function finds, for each observation, the minimum distance to an 
-#' observation of higher local density.
+#' @param distance a distance matrix.
+#' @param rho a vector of local density values as outputted by \code{\link{localDensity}}.
 #' 
-#' @param distance A distance matrix
+#' @return A vector of distances with index matching the index in \code{rho}.
 #' 
-#' @param rho A vector of local density values as outputted by localDensity()
-#' 
-#' @return A vector of distances with index matching the index in rho
-#' 
-#' @noRd
-#' 
+#' @export 
 distanceToPeak <- function(distance, rho) {
     comb <- as.matrix(distance)
     res <- sapply(1:length(rho), function(i) {
@@ -58,32 +51,29 @@ distanceToPeak <- function(distance, rho) {
     names(res) <- names(rho)
     res
 }
+
+
 #' Estimate the distance cutoff for a specified neighbor rate
 #' 
 #' This function calculates a distance cutoff value for a specific distance 
-#' matrix that makes the average neighbor rate (number of points within the
-#' distance cutoff value) fall between the provided range. The authors of the
+#' matrix that makes the average neighbor rate (number of points within the 
+#' distance cutoff value) fall between the provided range. The authors of the 
 #' algorithm suggests aiming for a neighbor rate between 1 and 2 percent, but 
-#' also states that the algorithm is quite robust with regards to more extreme
+#' also state that the algorithm is quite robust with regards to more extreme 
 #' cases.
 #' 
-#' @param distance A distance matrix
-#' 
-#' @param neighborRateLow The lower bound of the neighbor rate
-#' 
-#' @param neighborRateHigh The upper bound of the neighbor rate
-#' 
-#' @return A numeric value giving the estimated distance cutoff value
-#' 
+#' @param distance a distance matrix.
+#' @param neighborRateLow the lower bound of the neighbor rate.
+#' @param neighborRateHigh the upper bound of the neighbor rate.
+#'
+#' @return A numeric value giving the estimated best distance cutoff value.
+#'
 #' @examples
 #' irisDist <- dist(iris[,1:4])
-#' estimateDc(irisDist)
-#' 
-#' @references Rodriguez, A., & Laio, A. (2014). Clustering by fast search and find of density peaks. Science, 344(6191), 1492-1496. doi:10.1126/science.1242072
-#' 
+#' estimateDC(irisDist)
+#'   
 #' @export
-#' 
-estimateDc <- function(distance, neighborRateLow=0.01, neighborRateHigh=0.02) {
+estimateDC <- function(distance, neighborRateLow=0.01, neighborRateHigh=0.02) {
     comb <- as.matrix(distance)
     size <- attr(distance, 'Size')
     dc <- min(distance)
@@ -97,46 +87,45 @@ estimateDc <- function(distance, neighborRateLow=0.01, neighborRateHigh=0.02) {
         }
         dc <- dc + dcMod
     }
-    cat('Distance cutoff calculated to', dc, '\n')
+    cat('Distance cutoff calculated as', dc,'\n')
     dc
 }
-#' Calculate clustering attributes based on the densityClust algorithm
+
+
+#' Calculate clustering attributes
 #' 
 #' This function takes a distance matrix and optionally a distance cutoff and 
 #' calculates the values necessary for clustering based on the algorithm 
-#' proposed by Alex Rodrigues and Alessandro Laio (see references). The actual 
-#' assignment to clusters are done in a later step, based on user defined 
+#' proposed by Alex Rodrigues and Alessandro Laio (see \code{CITATION}). The 
+#' actual assignment to clusters is done in a later step, based on user defined 
 #' threshold values.
 #' 
-#' @details
-#' The function calculates rho and delta for the observations in the provided 
-#' distance matrix. If a distance cutoff is not provided this is first estimated
-#' using \code{\link{estimateDc}} with default values.
-#' 
-#' The information kept in the densityCluster object is:
-#' \describe{
-#'   \item{rho}{A vector of local density values}
-#'   \item{delta}{A vector of minimum distances to observations of higher density}
-#'   \item{distance}{The initial distance matrix}
-#'   \item{dc}{The distance cutoff used to calculate rho}
-#'   \item{threshold}{A named vector specifying the threshold values for rho and delta used for cluster detection}
-#'   \item{peaks}{A vector of indexes specifying the cluster center for each cluster}
-#'   \item{clusters}{A vector of cluster affiliations for each observation. The clusters are referenced as indexes in the peaks vector}
-#'   \item{halo}{A logical vector specifying for each observation if it is considered part of the halo}
-#' }
-#' Before running findClusters the threshold, peaks, clusters and halo data is 
-#' NA.
-#' 
-#' @param distance A distance matrix
-#' 
-#' @param dc A distance cutoff for calculating the local density. If missing it
-#' will be estimated with estimateDc(distance)
-#' 
-#' @param gaussian Logical. Should a gaussian kernel be used to estimate the 
-#' density (defaults to FALSE)
-#' 
-#' @return A densityCluster object. See details for a description.
-#' 
+#' @details The function calculates \eqn{\rho} and \eqn{\delta} for the 
+#'   observations in the provided distance matrix. If a distance cutoff is not 
+#'   provided this is first estimated using \code{\link{estimateDC}} with 
+#'   default values.
+#'   
+#'   The information kept in the densityCluster object is: \describe{ 
+#'   \item{rho}{a vector of local density values.} \item{delta}{a vector of 
+#'   minimum distances to observations of higher density.} \item{distance}{the 
+#'   initial distance matrix.} \item{dc}{the distance cutoff used to calculate 
+#'   rho.} \item{threshold}{a named vector specifying the threshold values for 
+#'   rho and delta used for cluster detection.} \item{peaks}{a vector of indices
+#'   specifying the cluster center for each cluster.} \item{clusters}{a vector 
+#'   of cluster affiliations for each point, with the clusters being referenced as 
+#'   indices in the peaks vector.} \item{halo}{a logical vector specifying for 
+#'   each observation if it is considered part of the halo.} } Before running 
+#'   \code{\link{findClusters}} the threshold, peaks, clusters and halo data is 
+#'   \code{NA}.
+#'   
+#' @param distance a distance matrix.
+#' @param dc a distance cutoff for calculating the local density. If missing it 
+#'   will be estimated with \code{\link{estimateDC}(distance)}.
+#' @param gaussian logical indicating that a Gaussian kernel should be used to
+#'   estimate the density.
+#'   
+#' @return A densityCluster object. See Details for a description.
+#'   
 #' @examples
 #' irisDist <- dist(iris[,1:4])
 #' irisClust <- dclust(irisDist, gaussian=TRUE)
@@ -146,15 +135,12 @@ estimateDc <- function(distance, neighborRateLow=0.01, neighborRateHigh=0.02) {
 #' plotMDS(irisClust)
 #' split(iris[,5], irisClust$clusters)
 #' 
-#' @seealso \code{\link{estimateDc}}, \code{\link{findClusters}}
-#' 
-#' @references Rodriguez, A., & Laio, A. (2014). Clustering by fast search and find of density peaks. Science, 344(6191), 1492-1496. doi:10.1126/science.1242072
-#' 
+#' @seealso \code{\link{estimateDC}}, \code{\link{findClusters}}
+#'   
 #' @export
-#' 
 dclust <- function(distance, dc, gaussian=FALSE) {
     if(missing(dc)) {
-        dc <- estimateDc(distance)
+        dc <- estimateDC(distance)
     }
     rho <- localDensity(distance, dc, gaussian=gaussian)
     delta <- distanceToPeak(distance, rho)
