b0eeba5eddcbc568f302814625cd1a97d54d8db6
Sean Hughes <smhughes@uw.edu>
2015-08-12 19:37:17 -0700

    Add tests to compare to reference implementation.

---
+++ tests/testthat/generateReference.R
@@ -0,0 +1,33 @@
+# This code was run with the initial reference implementation (i.e. https://github.com/thomasp85/densityClust/commit/b038fb30ea6f59d60a3a4b45eaa3ac9a504951f6)
+# It generates the .RData files with the expected results based on that 
+# implementation, which are then tested against with new implementations. 
+
+set.seed(123)
+dists <- list(
+   dist(matrix(rnorm(1000), ncol = 4)),
+   dist(matrix(rnorm(1000), ncol = 20)),
+   dist(matrix(rnorm(10000), ncol = 40)),
+   dist(matrix(sample(1:100000, 1000), ncol = 4)),
+   dist(matrix(sample(1:100000, 1000), ncol = 20)),
+   dist(matrix(sample(1:100000, 1000), ncol = 50))
+)
+
+# non-Gaussian
+densityClustReference <- lapply(dists, densityClust)
+save(densityClustReference, file = "tests/testthat/testdata/densityClustReference.RData")
+
+estimateDcReference <- lapply(dists, estimateDc)
+save(estimateDcReference, file = "tests/testthat/testdata/estimateDcReference.RData")
+
+localDensityReference <- Map(densityClust:::localDensity, dists, estimateDcReference)
+save(localDensityReference, file = "tests/testthat/testdata/localDensityReference.RData")
+
+distanceToPeakReference <- Map(densityClust:::distanceToPeak, dists, localDensityReference)
+save(distanceToPeakReference, file = "tests/testthat/testdata/distanceToPeakReference.RData")
+
+# Gaussian
+gaussianDensityClustReference <- lapply(dists, FUN = function(x) densityClust(x, gaussian = TRUE))
+save(gaussianDensityClustReference, file = "tests/testthat/testdata/gaussianDensityClustReference.RData")
+
+gaussianLocalDensityReference <- Map(f = function(x, y) densityClust:::localDensity(x, y, gaussian = TRUE), dists, estimateDcReference)
+save(gaussianLocalDensityReference, file = "tests/testthat/testdata/gaussianLocalDensityReference.RData")
---
+++ tests/testthat/testEquivalenceToReferenceImplementation.R
@@ -0,0 +1,48 @@
+set.seed(123)
+dists <- list(
+   dist(matrix(rnorm(1000), ncol = 4)),
+   dist(matrix(rnorm(1000), ncol = 20)),
+   dist(matrix(rnorm(10000), ncol = 40)),
+   dist(matrix(sample(1:100000, 1000), ncol = 4)),
+   dist(matrix(sample(1:100000, 1000), ncol = 20)),
+   dist(matrix(sample(1:100000, 1000), ncol = 50))
+)
+
+context("Reference implementation")
+
+load("testdata/densityClustReference.RData")
+densityClustNewImp <- lapply(dists, densityClust)
+test_that("Test equivalence to reference implementation of densityClust", {
+   expect_identical(densityClustReference, densityClustNewImp)   
+})
+
+load("testdata/estimateDcReference.RData")
+estimateDcNewImp <- lapply(dists, estimateDc)
+test_that("Test equivalence to reference implementation of estimateDc", {
+   expect_identical(estimateDcReference, estimateDcNewImp)   
+})
+
+load("testdata/localDensityReference.RData")
+localDensityNewImp <- Map(densityClust:::localDensity, dists, estimateDcNewImp)
+test_that("Test equivalence to reference implementation of localDensity", {
+   expect_identical(localDensityReference, localDensityNewImp)
+})
+
+load("testdata/distanceToPeakReference.RData")
+distanceToPeakNewImp <- Map(densityClust:::distanceToPeak, dists, localDensityNewImp)
+test_that("Test equivalence to reference implementation of distanceToPeak", {
+   expect_identical(distanceToPeakReference, distanceToPeakNewImp)
+})
+
+load("testdata/gaussianDensityClustReference.RData")
+gaussianDensityClustNewImp <- lapply(dists, FUN = function(x) densityClust(x, gaussian = TRUE))
+test_that("Test equivalence to reference implementation of gaussianDensityClust", {
+   expect_identical(gaussianDensityClustReference, gaussianDensityClustNewImp)   
+})
+
+load("testdata/gaussianLocalDensityReference.RData")
+gaussianLocalDensityNewImp <- Map(f = function(x, y) densityClust:::localDensity(x, y, gaussian = TRUE), dists, estimateDcReference)
+test_that("Test equivalence to reference implementation of localDensity", {
+   expect_identical(gaussianLocalDensityReference, gaussianLocalDensityNewImp)
+})
+
